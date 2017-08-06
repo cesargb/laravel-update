ee5c62f852266214aaec4cf3a63c2e519c7dfa3b
Cesar Garcia <cesargb@gmail.com>
2017-08-06 13:19:11 +0000

    Apply fixes from StyleCI

--- src/Notifications/Updated.php
+++ src/Notifications/Updated.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace Cesargb\Update\Notifications;
 
-use Cesargb\Update\Notifications\BaseNotification;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Messages\SlackAttachment;
+use Illuminate\Notifications\Messages\SlackMessage;
 
 class Updated extends BaseNotification
 {
@@ -17,12 +17,12 @@ class Updated extends BaseNotification
      */
     public function __construct($packages_updated)
     {
-        $this->packages_updated=$packages_updated;
+        $this->packages_updated = $packages_updated;
     }
 
     public function toMail(): MailMessage
     {
-        $mailMessage = (new MailMessage)
+        $mailMessage = (new MailMessage())
             ->subject($this->applicationName().' had updated '.count($this->packages_updated).' packages')
             ->line($this->applicationName().' had updated this '.count($this->packages_updated).' packages:');
 
@@ -32,16 +32,17 @@ class Updated extends BaseNotification
 
         return $mailMessage;
     }
+
     public function toSlack(): SlackMessage
     {
-        return (new SlackMessage)
+        return (new SlackMessage())
             ->to(config('update.notifications.slack.channel'))
             ->content($this->applicationName().' had updated '.count($this->packages_updated).' packages')
             ->attachment(function (SlackAttachment $attachment) {
                 foreach ($this->packages_updated as $update) {
                     $attachment->fields([
                         'package'   => $update['package'],
-                        'version'   => $update['version']
+                        'version'   => $update['version'],
                     ]);
                 }
             });
